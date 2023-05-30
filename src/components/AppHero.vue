83a2ae09dba8d5b94871e35dd32dd1febfea65c1
giuliabruno05 <giuliabruno48@gmail.com>
2023-05-30 16:44:15 +0200

    first commit

---
+++ src/components/AppHero.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "AppHero"
+}
+</script>
+
+<template>
+    <div>
+        <img src="/hero.webp" alt="img hero">
+    </div>
+</template>
+
+<style scoped></style>
